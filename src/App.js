2c525f9f239fe2153d8ae1331e99ff7eaea74675
Artur <54365622+bunge12@users.noreply.github.com>
2020-07-16 16:10:20 -0400

    Question generator created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Game from "./components/Game";
 
 function App() {
+  const [started, setStarted] = useState(null);
+  const [questions, setQuestions] = useState(null);
+
   return (
     <div className="App">
       <h1>country quiz</h1>
